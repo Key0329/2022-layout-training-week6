ebb1a181be7aba7f40dbf06dd49ddc263f3e6013
Key <springfield0329@gmail.com>
2022-08-17 02:23:01 +0800

    programming finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,37 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// 學習程式swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 8,
+      spaceBetween: 10,
+    },
+    1440: {
+      slidesPerView: 10,
+      spaceBetween: 0,
+    },
+  },
 });
