16bd15b8224ac06cee47504551a46c10a6a10e5f
Key <springfield0329@gmail.com>
2022-08-19 16:14:43 +0800

    class.html finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -5,10 +5,6 @@ const swiper1 = new Swiper(".Swiper1", {
   spaceBetween: 10,
   freeMode: true,
   loop: true,
-  pagination: {
-    el: ".swp1",
-    clickable: true,
-  },
   breakpoints: {
     375: {
       slidesPerView: 2,
@@ -63,3 +59,37 @@ const swiper2 = new Swiper('.swiper2', {
 
 });
 
+// class swiper
+
+const swiper3 = new Swiper(".Swiper3", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  freeMode: true,
+  loop: true,
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 10,
+    },
+    1440: {
+      slidesPerView: 6,
+      spaceBetween: 0,
+    },
+  },
+});
