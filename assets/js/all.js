27fd31f7ed0e0fbbe4d58a42692a8e7a9897fcc5
Key <springfield0329@gmail.com>
2022-08-19 16:15:14 +0800

    Update 2022-08-19T08:15:11.325Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,15 +1,11 @@
 "use strict";
 
 // 學習程式swiper
-var swiper = new Swiper(".mySwiper", {
+var swiper1 = new Swiper(".Swiper1", {
   slidesPerView: 2,
   spaceBetween: 10,
   freeMode: true,
   loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
   breakpoints: {
     375: {
       slidesPerView: 2,
@@ -33,6 +29,58 @@ var swiper = new Swiper(".mySwiper", {
     },
     1440: {
       slidesPerView: 10,
+      spaceBetween: 0
+    }
+  }
+}); // review swiper
+
+var swiper2 = new Swiper('.swiper2', {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  // 分頁   
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  // 左右箭頭    
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  mousewheel: true,
+  keyboard: true,
+  loop: true
+}); // class swiper
+
+var swiper3 = new Swiper(".Swiper3", {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  freeMode: true,
+  loop: true,
+  breakpoints: {
+    375: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    576: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 10
+    },
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 10
+    },
+    1440: {
+      slidesPerView: 6,
       spaceBetween: 0
     }
   }
